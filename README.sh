e8df29f9a6913a6c73c6913c99df496647033be7
nadavski <76551516+nadavski@users.noreply.github.com>
2026-09-10 11:11:39 +0700

    Delete README.sh

--- README.sh
+++
@@ -1,5 +0,0 @@
-sudo install -d -m 0755 /opt/nocturne-atelier
-sudo tar --no-same-owner -xzf /tmp/prod.tar.gz -C /opt/nocturne-atelier
-cd /opt/nocturne-atelier
-sudo chmod 0755 deploy.sh
-sudo ./deploy.sh
